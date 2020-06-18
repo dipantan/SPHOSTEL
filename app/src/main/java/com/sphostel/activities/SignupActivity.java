0ea0664e0267aa25a0e0b4905d8e5cc719c97259
Dipantan <sanju.mahinagar@gmail.com>
2020-06-18 07:19:00 -0700

    Added email & password authentication

--- app/src/main/java/com/sphostel/activities/SignupActivity.java
+++ app/src/main/java/com/sphostel/activities/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.sphostel.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -9,8 +10,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -19,7 +22,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.sphostel.R;
 import com.sphostel.models.Student;
@@ -27,17 +30,17 @@ import com.sphostel.utils.PatternField;
 
 import java.io.IOException;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
 public class SignupActivity extends AppCompatActivity {
     TextInputLayout sName, sRoll, sRoom, sDOB, sMobile, sEmail, sEmergency, sBlood, sPass;
-    String stName, stRoll, stRoom, stDOB, stMobile, stEmail, stEmergency, stBlood, stPass, stDept, academicYear, uuid;
+    //  String stName, stRoll, stRoom, stDOB, stMobile, stEmail, stEmergency, stBlood, stPass, stDept, academicYear;
+    String academicYear;
     Spinner sDept;
     RadioGroup sYear;
     Button signUp;
-    DatabaseReference reference;
-    CircleImageView circleImage;
-    FirebaseAuth firebaseAuth;
+    ImageView circleImage;
+    //   FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+    //   Student student;
     //  String imagePath;
     Uri uri;
     private int IMAGE_REQUEST = 1;
@@ -46,20 +49,8 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        circleImage = findViewById(R.id.circleImage);
-        sName = findViewById(R.id.text_input_name);
-        sRoll = findViewById(R.id.roll);
-        sRoom = findViewById(R.id.room);
-        sDOB = findViewById(R.id.dob);
-        sMobile = findViewById(R.id.mobile);
-        sEmail = findViewById(R.id.email);
-        sBlood = findViewById(R.id.blood);
-        sEmergency = findViewById(R.id.emergency_no);
-        sPass = findViewById(R.id.password);
-        sDept = findViewById(R.id.spinnerDept);
-        signUp = findViewById(R.id.signup);
-        sYear = findViewById(R.id.radioGroup);
-        firebaseAuth = FirebaseAuth.getInstance();
+        initView();
+        progressDialog = new ProgressDialog(this);
         circleImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,34 +82,125 @@ public class SignupActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!validatePassword() | !validateRest()) {
-                    return;
-                } else if (!stEmail.isEmpty() && !stPass.isEmpty()) {
-                    registerUser();
-                }
-                stDept = sDept.getSelectedItem().toString();
-                uuid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                reference = FirebaseDatabase.getInstance().getReference("students").child(academicYear);
-                Student student = new Student(stName, stDept, academicYear, stRoll, stRoom, stDOB, stMobile, stEmail, stEmergency, stBlood);
-                //   String roll = stRoll.replace("/","");
-                reference.child(stDept).child(uuid).setValue(student);
-                startActivity(new Intent(SignupActivity.this, DummyActivity.class));
+                registerUser();
             }
         });
     }
 
     private void registerUser() {
-        String email = sEmail.getEditText().getText().toString().trim();
-        String password = sPass.getEditText().getText().toString().trim();
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(SignupActivity.this, "Registration Success", Toast.LENGTH_SHORT).show();
+        final String stName = sName.getEditText().getText().toString().trim();
+        final String stRoll = sRoll.getEditText().getText().toString().trim();
+        final String stRoom = sRoom.getEditText().getText().toString().trim();
+        final String stDOB = sDOB.getEditText().getText().toString().trim();
+        final String stMobile = sMobile.getEditText().getText().toString().trim();
+        final String stEmail = sEmail.getEditText().getText().toString().trim();
+        final String stBlood = sBlood.getEditText().getText().toString().trim();
+        final String stEmergency = sEmergency.getEditText().getText().toString().trim();
+        final String stPass = sPass.getEditText().getText().toString().trim();
+        final String department = sDept.getSelectedItem().toString();
+        final String year = academicYear;
+        //  if(user != null){
+
+        //   }
+        if (stName.isEmpty()) {
+            sName.setError("Name cant be empty");
+            sName.requestFocus();
+            return;
+        }
+        if (stRoll.isEmpty()) {
+            sRoll.setError("Roll cant be empty");
+            sRoll.requestFocus();
+            return;
+        }
+        if (stRoom.isEmpty()) {
+            sRoom.setError("Name cant be empty");
+            sRoom.requestFocus();
+            return;
+        }
+        if (stDOB.isEmpty()) {
+            sDOB.setError("Name cant be empty");
+            sDOB.requestFocus();
+            return;
+        }
+        if (stMobile.isEmpty()) {
+            sMobile.setError("Name cant be empty");
+            sMobile.requestFocus();
+            return;
+        }
+        if (stEmail.isEmpty()) {
+            sEmail.setError("Name cant be empty");
+            sEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(stEmail).matches()) {
+            sEmail.setError("Enter a valid email");
+            sEmail.requestFocus();
+            return;
+        }
+        if (stEmergency.isEmpty()) {
+            sEmergency.setError("Name cant be empty");
+            sEmergency.requestFocus();
+            return;
+        }
+        if (stBlood.isEmpty()) {
+            sBlood.setError("Name cant be empty");
+            sBlood.requestFocus();
+            return;
+        }
+        if (stPass.isEmpty()) {
+            sPass.setError("Name cant be empty");
+            sPass.requestFocus();
+            return;
+        }
+        if (!PatternField.PASSWORD_PATTERN.matcher(stPass).matches()) {
+            sPass.setError("Name cant be empty");
+            sPass.requestFocus();
+            return;
+        }
+        progressDialog.setMessage("Loading");
+        progressDialog.show();
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(stEmail, stPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Student student = new Student(
+                        stName,
+                        department,
+                        year,
+                        stRoll,
+                        stRoom,
+                        stDOB,
+                        stMobile,
+                        stEmail,
+                        stEmergency,
+                        stBlood
+                );
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                FirebaseUser user = auth.getCurrentUser();
+                final String UUID = user.getUid();
+                //   String UUID;
+                try {
+                    FirebaseDatabase.getInstance().getReference("students").
+                            child(year).
+                            child(department).
+                            child(UUID).
+                            setValue(student).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            progressDialog.dismiss();
+                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                            Toast.makeText(SignupActivity.this, "Please login", Toast.LENGTH_SHORT).show();
+
                         }
-                    }
-                });
+                    });
+                } catch (NullPointerException ignored) {
+
+                }
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(SignupActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -136,55 +218,22 @@ public class SignupActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-
     }
 
-    //validate rest
-    boolean validateRest() {
-        stName = sName.getEditText().getText().toString().trim();
-        stRoll = sRoll.getEditText().getText().toString().trim();
-        stRoom = sRoom.getEditText().getText().toString().trim();
-        stDOB = sDOB.getEditText().getText().toString().trim();
-        stMobile = sMobile.getEditText().getText().toString().trim();
-        stEmail = sEmail.getEditText().getText().toString().trim();
-        stBlood = sBlood.getEditText().getText().toString().trim();
-        stEmergency = sEmergency.getEditText().getText().toString().trim();
-        if (stName.isEmpty() && stRoll.isEmpty() && stRoom.isEmpty() && stDOB.isEmpty() && stMobile.isEmpty() && stEmail.isEmpty() && stEmergency.isEmpty() && stBlood.isEmpty()) {
-            sName.setError("Field cant be empty");
-            sRoll.setError("Field cant be empty");
-            sRoom.setError("Field cant be empty");
-            sDOB.setError("Field cant be empty");
-            sMobile.setError("Field cant be empty");
-            sEmail.setError("Field cant be empty");
-            sEmergency.setError("Field cant be empty");
-            sBlood.setError("Field cant be empty");
-            return false;
-        } else {
-            sName.setError(null);
-            sRoll.setError(null);
-            sRoom.setError(null);
-            sDOB.setError(null);
-            sMobile.setError(null);
-            sEmail.setError(null);
-            sEmergency.setError(null);
-            sBlood.setError(null);
-            return true;
-        }
-    }
-
-    //validate password
-    boolean validatePassword() {
-        stPass = sPass.getEditText().getText().toString().trim();
-        if (stPass.isEmpty()) {
-            sPass.setError("Field cant be empty");
-            return false;
-        } else if (!PatternField.PASSWORD_PATTERN.matcher(stPass).matches()) {
-            sPass.setError("Password too weak");
-            return false;
-        } else {
-            sPass.setError(null);
-            return true;
-        }
+    public void initView() {
+        circleImage = findViewById(R.id.circleImage);
+        sName = findViewById(R.id.text_input_name);
+        sRoll = findViewById(R.id.roll);
+        sRoom = findViewById(R.id.room);
+        sDOB = findViewById(R.id.dob);
+        sMobile = findViewById(R.id.mobile);
+        sEmail = findViewById(R.id.email);
+        sBlood = findViewById(R.id.blood);
+        sEmergency = findViewById(R.id.emergency_no);
+        sPass = findViewById(R.id.password);
+        sDept = findViewById(R.id.spinnerDept);
+        signUp = findViewById(R.id.signup);
+        sYear = findViewById(R.id.radioGroup);
     }
 
 }
