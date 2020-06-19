b210d53ce850888af8cc2bcd1c2f370de4f7fab5
Dipantan <sanju.mahinagar@gmail.com>
2020-06-19 04:44:02 -0700

    Initial commit

--- app/src/main/java/com/sphostel/activities/HomeActivity.java
+++ app/src/main/java/com/sphostel/activities/HomeActivity.java
@@ -1,28 +1,63 @@
 package com.sphostel.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sphostel.R;
 
-public class HomeActivity extends AppCompatActivity {
-    private TextView textvView;
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         initView();
-
-        Toast.makeText(this, "Welocome to homeactivity", Toast.LENGTH_SHORT).show();
-        // TextView textView = new TextView(this);
-
+        Toast.makeText(this, "Welcome to HomeActivity", Toast.LENGTH_SHORT).show();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = user.getEmail();
+        mEmail.setText(email);
     }
 
     private void initView() {
-        textvView = findViewById(R.id.t);
+        Button mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+        mEmail = findViewById(R.id.email);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Are you sure").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth firebaseAuth;
+                        firebaseAuth = FirebaseAuth.getInstance();
+                        firebaseAuth.signOut();
+                        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                break;
+            default:
+                break;
+        }
+    }
 }
