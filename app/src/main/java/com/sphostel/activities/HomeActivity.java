f39a20f4b8efac1e13789d6bd9e21953228465f7
Dipantan <sanju.mahinagar@gmail.com>
2020-06-19 10:15:48 -0700

    Added profile image from firebase cloud storage

--- app/src/main/java/com/sphostel/activities/HomeActivity.java
+++ app/src/main/java/com/sphostel/activities/HomeActivity.java
@@ -6,32 +6,65 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sphostel.R;
+import com.sphostel.models.Student;
+//import com.sphostel.models.Student;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView mEmail;
+    private ImageView mProfile;
+    //   List<Student> students;
+    // Student student;
+    // private static final String FIREBASE_STORAGE_URL = "gs://sphostel-3ca65.appspot.com/images/profiles/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ///     students = new ArrayList<>();
         initView();
-        Toast.makeText(this, "Welcome to HomeActivity", Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, "Welcome to HomeActivity", Toast.LENGTH_SHORT).show();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String email = user.getEmail();
-        mEmail.setText(email);
+        final String UUID = user.getUid();
+        //setting user profile image
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("students").child(UUID);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String student = dataSnapshot.getValue(Student.class).getPhotoUrl();
+                try {
+                    Glide.with(HomeActivity.this).load(student).into(mProfile);
+                } catch (NullPointerException ignored) {
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //  Log.d("URl:", student.getPhotoUrl());
     }
 
     private void initView() {
-        Button mButton = findViewById(R.id.button);
+        ImageButton mButton = findViewById(R.id.button);
         mButton.setOnClickListener(this);
-        mEmail = findViewById(R.id.email);
+        //   TextView mEmail = findViewById(R.id.email);
+        mProfile = findViewById(R.id.profile);
     }
 
     @Override
