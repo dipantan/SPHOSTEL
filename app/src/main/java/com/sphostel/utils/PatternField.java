4985eb588d9ba1d0fe3c7dbbf74b14c3c5909a65
Dipantan <sanju.mahinagar@gmail.com>
2020-06-26 10:21:07 -0700

    Added Roll pattern

--- app/src/main/java/com/sphostel/utils/PatternField.java
+++ app/src/main/java/com/sphostel/utils/PatternField.java
@@ -7,6 +7,14 @@ import java.util.regex.Pattern;
  */
 
 public class PatternField {
+    public static final Pattern ROLL_NO
+            = Pattern.compile(
+            "[A-Z]{1,3}" +     //must contains characters A-Z
+                    "/" +     //then a /
+                    "[0-9]{1,3}" +    //then digits
+                    "/" +     //then a /
+                    "[0-9]{1,3}"    //then digits
+    );
     public static final Pattern PASSWORD_PATTERN =
             Pattern.compile("^" +           //  pattern starts
                     "(?=.*[0-9])" +         //  at least 1 digit
@@ -16,6 +24,4 @@ public class PatternField {
                     "(?=\\S+$)" +           //  no white spaces
                     ".{6,}" +               //  at least 6 characters
                     "$");                   //  pattern ends
-
-
 }
