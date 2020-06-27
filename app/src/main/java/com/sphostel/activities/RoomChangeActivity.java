c558cb19f4c6befdae01c0af1177f27a4509b521
Dipantan <sanju.mahinagar@gmail.com>
2020-06-27 10:32:10 -0700

    Initial commit

--- app/src/main/java/com/sphostel/activities/RoomChangeActivity.java
+++ app/src/main/java/com/sphostel/activities/RoomChangeActivity.java
@@ -11,7 +11,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.sphostel.R;
-import com.sphostel.models.Rooms;
+import com.sphostel.models.Student;
 
 public class RoomChangeActivity extends AppCompatActivity {
     String sName, sDept, sYear, sRoll, sMobile, sCRoom, sNRoom, SReason;
@@ -31,12 +31,12 @@ public class RoomChangeActivity extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                sName = dataSnapshot.getValue(Rooms.class).getsName();
-                sDept = dataSnapshot.getValue(Rooms.class).getsDept();
-                sYear = dataSnapshot.getValue(Rooms.class).getsYear();
-                sRoll = dataSnapshot.getValue(Rooms.class).getsRoll();
-                sMobile = dataSnapshot.getValue(Rooms.class).getsMobile();
-                sCRoom = dataSnapshot.getValue(Rooms.class).getsCRoom();
+                sName = dataSnapshot.getValue(Student.class).getName();
+                sDept = dataSnapshot.getValue(Student.class).getDepartment();
+                sYear = dataSnapshot.getValue(Student.class).getAcademicYear();
+                sRoll = dataSnapshot.getValue(Student.class).getRoll_no();
+                sMobile = dataSnapshot.getValue(Student.class).getMobile_no();
+                sCRoom = dataSnapshot.getValue(Student.class).getRoom_no();
                 setValues();
             }
 
@@ -52,7 +52,7 @@ public class RoomChangeActivity extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                //  Rooms rooms = new Rooms()
             }
 
             @Override
--- app/src/main/java/com/sphostel/models/Rooms.java
+++ app/src/main/java/com/sphostel/models/Rooms.java
@@ -5,19 +5,21 @@ package com.sphostel.models;
  */
 
 public class Rooms {
-    String sName, sDept, sYear, sRoll, sMobile, sCRoom;
+    String sName, sDept, sYear, sRoll, sMobile, sCRoom, sNRoom, sReason;
 
     public Rooms() {
         //empty constructor for receiving fireBase objects
     }
 
-    public Rooms(String sName, String sDept, String sYear, String sRoll, String sMobile, String sCRoom) {
+    public Rooms(String sName, String sDept, String sYear, String sRoll, String sMobile, String sCRoom, String sNRoom, String sReason) {
         this.sName = sName;
         this.sDept = sDept;
         this.sYear = sYear;
         this.sRoll = sRoll;
         this.sMobile = sMobile;
         this.sCRoom = sCRoom;
+        this.sNRoom = sNRoom;
+        this.sReason = sReason;
     }
 
     public String getsName() {
@@ -43,4 +45,13 @@ public class Rooms {
     public String getsCRoom() {
         return sCRoom;
     }
+
+    public String getsNRoom() {
+        return sNRoom;
+    }
+
+    public String getsReason() {
+        return sReason;
+    }
+
 }
