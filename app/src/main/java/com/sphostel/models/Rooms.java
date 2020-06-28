d76235ab8ac66112936521eb83c87ae0f37730d8
Dipantan <sanju.mahinagar@gmail.com>
2020-06-28 03:46:14 -0700

    Added status as a field

--- app/src/main/java/com/sphostel/models/Rooms.java
+++ app/src/main/java/com/sphostel/models/Rooms.java
@@ -5,53 +5,93 @@ package com.sphostel.models;
  */
 
 public class Rooms {
-    String Name, Department, Year, Roll, Mobile, Current_Room, New_Room, Reason;
+    private String Name, Department, Year, Roll, Mobile, Current_Room, New_Room, Reason, Status;
 
     public Rooms() {
         //empty constructor for receiving fireBase objects
     }
 
-    public Rooms(String Name, String Department, String Year, String Roll, String Mobile, String Current_Room, String New_Room, String Reason) {
-        this.Name = Name;
-        this.Department = Department;
-        this.Year = Year;
-        this.Roll = Roll;
-        this.Mobile = Mobile;
-        this.Current_Room = Current_Room;
-        this.New_Room = New_Room;
-        this.Reason = Reason;
+    public Rooms(String name, String department, String year, String roll, String mobile, String current_Room, String new_Room, String reason, String status) {
+        Name = name;
+        Department = department;
+        Year = year;
+        Roll = roll;
+        Mobile = mobile;
+        Current_Room = current_Room;
+        New_Room = new_Room;
+        Reason = reason;
+        Status = status;
     }
 
     public String getName() {
         return Name;
     }
 
+    public void setName(String name) {
+        Name = name;
+    }
+
     public String getDepartment() {
         return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
     }
 
     public String getYear() {
         return Year;
     }
 
+    public void setYear(String year) {
+        Year = year;
+    }
+
     public String getRoll() {
         return Roll;
+    }
+
+    public void setRoll(String roll) {
+        Roll = roll;
     }
 
     public String getMobile() {
         return Mobile;
     }
 
+    public void setMobile(String mobile) {
+        Mobile = mobile;
+    }
+
     public String getCurrent_Room() {
         return Current_Room;
+    }
+
+    public void setCurrent_Room(String current_Room) {
+        Current_Room = current_Room;
     }
 
     public String getNew_Room() {
         return New_Room;
     }
 
+    public void setNew_Room(String new_Room) {
+        New_Room = new_Room;
+    }
+
     public String getReason() {
         return Reason;
     }
 
+    public void setReason(String reason) {
+        Reason = reason;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
 }
